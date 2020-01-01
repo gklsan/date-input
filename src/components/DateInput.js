5509d5bcc537e356234d435d124a5c8b0b05ae6f
Gokul <pgokulmca@gmail.com>
2020-01-02 01:56:12 +0530

    Initial commit

---
+++ src/components/DateInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class DateInput extends Component {
+  render() {
+    return <h1>Hi Gokul</h1>;
+  }
+}
+
+export default DateInput;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import DateInput from "./components/DateInput";
+
+console.log('test');
+
+export default DateInput;
+export { DateInput }
